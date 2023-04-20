0515d3220362107b1b27e66022f4371fba7fb2ba
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-20 20:07:10 +0300

    DZ_9_1/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-setInterval
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  const buttonStart = document.querySelector(".js-button-start");
+  const buttonStop = document.querySelector(".js-button-stop");
+  let colorSwitcher = null;
+
+  buttonStart.addEventListener("click", () => {
+    buttonStart.disabled = true;
+    colorSwitcher = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    }, 1000);
+  });
+  
+  buttonStop.addEventListener("click", () => {
+    clearInterval(colorSwitcher);
+    buttonStart.disabled = false;
+  });
